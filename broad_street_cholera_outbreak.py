90892e3499f02818ccb827260efadf9580920ec0
Gilles Pilon <gillespilon13@gmail.com>
2019-09-09 21:00:32 -0500

    Tidy long line of download data

--- broad_street_cholera_outbreak.py
+++ broad_street_cholera_outbreak.py
@@ -11,16 +11,21 @@ There was a severe outbreak of cholera in 1854 in the Soho district of London,
 England, which killed 616 people over one month. Dr. John Snow and Reverence
 Henry Whitehead linked the outbreak to contaminated water, identified the index
 case, and stopped the outbreak by having the Broad Street water pump disabled.
+
+[snow_cholera_deaths.csv](https://drive.google.com/
+open?id=0BzrdQfHR2I5DSE5NWFZlQXV5VnM).
+
+The x-y values are the distances in m from the lower left datum of the map.
+Each pair represents one death. There are 578 values, slightly less than the
+616 actual deaths.
+
+[snow_cholera_pumps.csv](https://drive.google.com/
+open?id=0BzrdQfHR2I5DSDd2emxObk9HUDA). The x-y values are the distances in m
+from the lower left datum of the map. Each pair represents one pump. There are
+13 values, representing 13 pumps.
 '''
 
 
-# ## Data
-#
-# Download the data sets.
-#
-# [snow_cholera_deaths.csv](https://drive.google.com/open?id=0BzrdQfHR2I5DSE5NWFZlQXV5VnM). The x-y values are the distances in m from the lower left datum of the map. Each pair represents one death. There are 578 values, slightly less than the 616 actual deaths.
-#
-# [snow_cholera_pumps.csv](https://drive.google.com/open?id=0BzrdQfHR2I5DSDd2emxObk9HUDA). The x-y values are the distances in m from the lower left datum of the map. Each pair represents one pump. There are 13 values, representing 13 pumps.
 
 # ## Methodology
 #
